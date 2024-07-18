cded78f45c65b8cbb54912f73837ea221cb9d2e6
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-07-18 20:51:12 +0900

    Title: 합을 복사하기, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240718/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =1;
+        int ta=a;
+        int b=2;
+        int tb=b;
+        int c=3;
+        int tc=c;
+        a=ta+tb+tc;
+        b=ta+tb+tc;
+        c=ta+tb+tc;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
