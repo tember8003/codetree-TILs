9c9478889b55dec5f52ebf72d1bf30f7545ab74b
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-07-20 01:12:15 +0900

    Title: 1로 만드는 연산, Time: 109ms, Memory: 11MB, Status: Passed - CodeTree

--- 240720/1로 만드는 연산/operation-to-1.java
+++ 240720/1로 만드는 연산/operation-to-1.java
@@ -7,23 +7,16 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		//int[] dp = new int[n+1];
-		//Arrays.fill(dp, 0);
-		int ans=0;
-		while(n!=1) {
-			if(n>=3 && n%3==0) {
-				n/=3;
-				ans++;
+		int[] dp = new int[n+1];
+		for(int i=2; i<=n; i++) {
+			dp[i]=dp[i-1]+1;
+			if(i%2==0) {
+				dp[i]=Math.min(dp[i], dp[i/2]+1);
 			}
-			else if(n>=2 && n%2==0) {
-				n/=2;
-				ans++;
-			}
-			else {
-				n--;
-				ans++;
+			if(i%3==0) {
+				dp[i]=Math.min(dp[i], dp[i/3]+1);
 			}
 		}
-		System.out.println(ans);
+		System.out.println(dp[n]);
 	}
 }
