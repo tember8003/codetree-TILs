bf760242fa078276dda13c7a80f61c2f0d88d6a3
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-07-23 01:51:59 +0900

    Title: 계단 오르기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240723/계단 오르기/climbing-stairs.java
+++ 240723/계단 오르기/climbing-stairs.java
@@ -12,8 +12,9 @@ public class Main {
 			System.out.println(1);
 		}
 		else {
-			for(int i=3; i<=n; i++) {
-				dp[i]=Math.max(dp[i-2], dp[i-3])%10007+1;
+			dp[0]=1; dp[1]=0; dp[2]=1; dp[3]=1;
+			for(int i=4; i<=n; i++) {
+				dp[i]=(dp[i-2]+dp[i-3])%10007;
 			}
 			System.out.println(dp[n]);
 		}
