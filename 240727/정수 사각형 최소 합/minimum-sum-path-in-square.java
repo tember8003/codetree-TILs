e49c2a5794a5f9f97a8c0803f11e33d4099d35f9
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-07-27 19:06:51 +0900

    Title: 정수 사각형 최소 합, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N+3][N+3];
+		int[][] dp = new int[N+3][N+3];
+		
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1; j<=N; j++) {
+				arr[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			dp[i][N]=dp[i-1][N]+arr[i][N];
+		}
+		for(int i=N-1; i>=1; i--) {
+			dp[1][i]= dp[1][i+1]+arr[1][i];
+		}
+		
+		for(int i=2; i<=N; i++) {
+			for(int j=N-1; j>=1; j--) {
+				dp[i][j] = Math.min(dp[i-1][j], dp[i][j+1])+arr[i][j];
+			}
+		}
+		System.out.println(dp[N][1]);
+	}
+}
