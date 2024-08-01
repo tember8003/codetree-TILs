6ef2cbd03b1287ca684aff2e7137645582ea7dbd
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-02 00:48:09 +0900

    Title: 나선형 삼각형, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240802/나선형 삼각형/spira-triangle.java
+++ 240802/나선형 삼각형/spira-triangle.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n+1];
+		long[] dp = new long[n+1];
 		
 		if(n<=3) {
 			System.out.println(1);
