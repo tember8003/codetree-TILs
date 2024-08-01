152f75308751c9b9b0aeb4d283b5409fa4889cdc
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-02 00:46:36 +0900

    Title: 나선형 삼각형, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240802/나선형 삼각형/spira-triangle.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	//dp문제
+	static int n;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n+1];
+		
+		if(n<=3) {
+			System.out.println(1);
+		}
+		else {
+			dp[1]=1; dp[2]=1; dp[3]=1;
+			for(int i=4; i<=n; i++) {
+				dp[i]=dp[i-2]+dp[i-3];
+			}
+			System.out.println(dp[n]);
+		}
+	}
+}
