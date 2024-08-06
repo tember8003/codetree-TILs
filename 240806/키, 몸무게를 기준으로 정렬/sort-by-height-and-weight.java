b18aefcdb984d6697b6ac13f4b33ae02e6cb6652
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-06 21:58:26 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Student implements Comparable<Student>{
+	String name;
+	int height;
+	int weight;
+	
+	public Student(String name, int height,int weight) {
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	public int compareTo(Student student) {
+		if(this.height == student.height) {
+			return student.weight - this.weight;
+		}
+		else {
+			return this.height - student.height;
+		}
+	}
+}
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		Student[] list = new Student[n];
+		StringTokenizer st;
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int height = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			list[i] = new Student(name, height, weight);
+		}
+		
+		Arrays.sort(list);
+		for(Student student : list) {
+			System.out.println(student.name+" "+student.height+" "+student.weight);
+		}
+	}
+}
