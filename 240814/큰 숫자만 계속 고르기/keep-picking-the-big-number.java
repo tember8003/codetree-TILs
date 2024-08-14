d0bd69d8a565e09801b7e883fedad24f14dc1d37
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-14 22:22:22 +0900

    Title: 큰 숫자만 계속 고르기, Time: 3033ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

---
+++ 240814/큰 숫자만 계속 고르기/keep-picking-the-big-number.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	//우선순위 큐 내림차순 문제 + 최댓값 찾기
+	
+	static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		st= new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+			pq.add(arr[i]);
+		}
+		
+		for(int i=0; i<m; i++) {
+			int max_num = pq.poll();
+			for(int j=0; j<n; j++) {
+				if(max_num==arr[j]) {
+					arr[j]--;
+					pq.add(arr[j]);
+				}
+			}
+		}
+		System.out.println(pq.peek());
+	}
+}
