290f71edee675dd37bedd0fcfe85535528f87dec
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-17 20:22:03 +0900

    Title: 서로 다른 숫자, Time: 363ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240817/서로 다른 숫자/distinct-numbers.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	//해시셋 문제
+	static HashSet<Integer> set = new HashSet<Integer>();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			set.add(num);
+		}
+		
+		System.out.println(set.size());
+	}
+}
