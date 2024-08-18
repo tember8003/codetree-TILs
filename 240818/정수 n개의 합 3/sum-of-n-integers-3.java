7f7d0b4b15d50e5d3253c2b241409be64ddf9ab5
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-18 23:32:51 +0900

    Title: 정수 n개의 합 3, Time: 246ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240818/정수 n개의 합 3/sum-of-n-integers-3.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Main {
+    //누적합 문제
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		k--;
+		
+		int[][] arr = new int[n+1][n+1];
+		int[][] sum = new int[n+1][n+1];
+		
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1; j<=n; j++) {
+				arr[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+arr[i][j];
+			}
+		}
+		
+		int max_num = 0;
+		for(int i=1; i<=n-k; i++) {
+			for(int j=1; j<=n-k; j++) {
+				max_num = Math.max(max_num, (sum[i+k][j+k] - sum[i-1][j+k] - sum[i+k][j-1] +sum[i-1][j-1]));
+			}
+		}
+		
+		System.out.println(max_num);
+	}
+}
