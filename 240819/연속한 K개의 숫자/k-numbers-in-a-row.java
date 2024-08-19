42f02a2cf8e8832add5170a0f05c9d9bbce6a43e
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-19 19:59:25 +0900

    Title: 연속한 K개의 숫자, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/연속한 K개의 숫자/k-numbers-in-a-row.java
+++ 240819/연속한 K개의 숫자/k-numbers-in-a-row.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    //누적합 문제
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
