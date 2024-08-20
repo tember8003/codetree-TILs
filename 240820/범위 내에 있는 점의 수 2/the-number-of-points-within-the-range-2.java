470d842db8c4a2a9ac51c6341b6cf2de2da1c0d0
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-20 17:53:11 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240820/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	//누적합 문제
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int Q = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[1000001];
+		int[] sum = new int[1000001];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[num]=1;
+		}
+		
+		for(int i=1; i<=1000000; i++) {
+			sum[i] = sum[i-1]+arr[i];
+		}
+		
+		for(int i=0; i<Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append((sum[b] - sum[a-1])+"\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
