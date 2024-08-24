28daaa623cdb2ce18a576de4530540a08735d612
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-24 15:03:53 +0900

    Title: 물건 구입 비용, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240824/물건 구입 비용/cost-of-purchase.java
+++ 240824/물건 구입 비용/cost-of-purchase.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Store{
+class Store implements Comparable<Store>{
 	int set;
 	int money;
 	
@@ -12,9 +12,14 @@ class Store{
 		this.set = set;
 		this.money =money;
 	}
+	
+	public int compareTo(Store s) {
+		return s.money - this.money;
+	}
 }
 public class Main {
 	//dp 문제
+	static int MAX_NUM = 1000000001;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,7 +29,7 @@ public class Main {
 		
 		Store[] store = new Store[p];
 		int[] dp = new int[n+1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
+		Arrays.fill(dp, MAX_NUM);
 		
 		for(int i=0; i<p; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -35,10 +40,13 @@ public class Main {
 		}
 		
 		dp[0]=0;
-		for(int i=1; i<=n; i++) {
+		for(int i=0; i<=n; i++) {
 			for(int j=0; j<p; j++) {
-				if(i - store[j].set >= 0) {
-					dp[i] = Math.min(dp[i], dp[i-store[j].set] + store[j].money);
+				if(i + store[j].set <= n) {
+					dp[i + store[j].set] = Math.min(dp[i + store[j].set], dp[i] + store[j].money);
+				}
+				else {
+					dp[n] = Math.min(dp[n], dp[i]+store[j].money);
 				}
 			}
 		}
