c315da6ff0875bebbfee8d44300ff8e6cca2a046
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-24 18:09:51 +0900

    Title: 차이가 가장 작은 수, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240824/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	static int MAX_NUM = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			arr[i]=num;
+		}
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = n-1;
+		
+		int ans = MAX_NUM;
+		while(left < right) {
+			if(arr[right] - arr[left] < m) {
+				left++;
+			}
+			else {
+				ans = Math.min(ans, arr[right]-arr[left]);
+				right--;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
