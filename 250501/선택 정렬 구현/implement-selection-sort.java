2d442e6c448c76eeebe56d155574d44b520c12bb
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-05-01 08:31:38 +0900

    Title: 선택 정렬 구현, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250501/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        for(int i=0; i<arr.length; i++){
+            int min = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
