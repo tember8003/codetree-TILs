003f26896b7afeeaa6988366aa88866bba1bd433
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-05-02 10:40:57 +0900

    Title: 삽입 정렬 구현, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250502/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        for(int i=1; i<arr.length; i++){
+            int j = i-1;
+            int key = arr[i];
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
